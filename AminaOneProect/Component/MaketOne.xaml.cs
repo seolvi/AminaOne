64ce666b52320b59c2b701c615ae76cbdf073615
AminaLesya <callmelesya@mail.ru>
2023-10-11 11:10:02 +0300

    added one

---
+++ AminaOneProect/Component/MaketOne.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AminaOneProect.Component
+{
+    /// <summary>
+    /// Логика взаимодействия для MaketOne.xaml
+    /// </summary>
+    public partial class MaketOne : UserControl
+    {
+        public MaketOne()
+        {
+            InitializeComponent();
+        }
+    }
+}
