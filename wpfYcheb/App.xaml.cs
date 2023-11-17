e1f0337979379f6acac7a4d8fb9698e5e0f44f12
AminaLesya <callmelesya@mail.ru>
2023-11-17 19:53:22 +0300

    add some stuff

---
+++ wpfYcheb/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using wpfYcheb.Components;
+using Application = System.Windows.Application;
+
+namespace wpfYcheb
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static wpf_ychebEntities db = new wpf_ychebEntities();
+    }
+}
